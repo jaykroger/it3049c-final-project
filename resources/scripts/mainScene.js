6538b20d32a30805ce553f5e7e6748e692a06809
jaykroger <143293170+jaykroger@users.noreply.github.com>
2024-07-28 16:20:12 -0400

    added carsList for car spawning implementation

--- resources/scripts/mainScene.js
+++ resources/scripts/mainScene.js
@@ -23,35 +23,97 @@ class mainScene extends Phaser.Scene {
       config.height,
       "highway"
     );
-
     this.background.setOrigin(0, 0);
 
     this.player = this.physics.add.sprite(487, 850, "cars", "lambo");
     this.player.setScale(2);
-    this.cursorKeys = this.input.keyboard.createCursorKeys();
     this.player.setCollideWorldBounds(true);
-  }
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-  movePlayerManager() {
-    this.player.setVelocity(0);
-
-    if (this.cursorKeys.left.isDown) {
-      this.player.setVelocityX(-300);
-    }
-    if (this.cursorKeys.right.isDown) {
-      this.player.setVelocityX(300);
-    }
-
-    if (this.cursorKeys.up.isDown) {
-      this.player.setVelocityY(-300);
-    }
-    if (this.cursorKeys.down.isDown) {
-      this.player.setVelocityY(300);
-    }
+    this.carList = [
+      "dumptruck",
+      "tow_truck",
+      "tow_truck2",
+      "tow_truck3",
+      "truck2",
+      "truck3",
+      "landcruiser",
+      "landcruiser2",
+      "landcruiser3",
+      "van",
+      "raptor",
+      "raptor2",
+      "pickup",
+      "pickup2",
+      "pickup3",
+      "suv",
+      "suv2",
+      "van2",
+      "van3",
+      "mustang2",
+      "camaro",
+      "camaro2",
+      "challenger2",
+      "challenger3",
+      "lexus",
+      "lexus2",
+      "gwagon",
+      "bmw",
+      "gwagon2",
+      "patrol",
+      "patrol2",
+      "lexus3",
+      "taxi",
+      "taxi2",
+      "lambo2",
+      "lancer",
+      "bmw2",
+      "bmw3",
+      "lancer2",
+      "mustang3",
+      "mini",
+      "tida2",
+      "tida3",
+      "convertible",
+      "figo",
+      "figo2",
+      "porsche",
+      "bike2",
+      "bike",
+    ];
   }
 
   update() {
     this.background.tilePositionY -= gameSettings.gameSpeed;
     this.movePlayerManager();
+  }
+
+  movePlayerManager() {
+    // Handle all player movement
+    // Reset player velocity and angle when no button is being pressed
+    this.player.setVelocity(0);
+    this.player.setAcceleration(0);
+    this.player.setAngle(0);
+
+    // Move player left and right and add rotation to simulate steering
+    if (this.cursorKeys.left.isDown) {
+      this.player.setAccelerationX(-15000);
+      this.player.setAngle(-10);
+    }
+    if (this.cursorKeys.right.isDown) {
+      this.player.setAccelerationX(15000);
+      this.player.setAngle(10);
+    }
+
+    if (this.cursorKeys.up.isDown) {
+      this.player.setAccelerationY(-15000);
+    }
+    if (this.cursorKeys.down.isDown) {
+      this.player.setAccelerationY(18000);
+    }
+  }
+
+  spawnCar() {
+    // Spawn a car at a random lane on the road
   }
 }
