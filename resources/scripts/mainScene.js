1b66f1b54895aa497d2f85a83e70c006842b017b
ballourl <135663136+FriarobbyCollege@users.noreply.github.com>
2024-07-29 15:58:03 -0400

    Added the traffic to the game, add different models to the game, and a way for the score to be tallied.

--- resources/scripts/mainScene.js
+++ resources/scripts/mainScene.js
@@ -96,6 +96,31 @@ class mainScene extends Phaser.Scene {
       "bike",
     ];
 
+
+
+    this.car1 = this.add.sprite(487, 100, "cars", "pickup");
+    this.car2 = this.add.sprite(487, 100, "cars", "pickup");
+    this.car3 = this.add.sprite(487, 100, "cars", "pickup");
+    this.car4 = this.add.sprite(487, 100, "cars", "pickup");
+
+    this.car1.setScale(2.5);
+    this.car2.setScale(2.5);
+    this.car3.setScale(2.5);
+    this.car4.setScale(2.5);
+
+    this.car1.setInteractive();
+    this.car2.setInteractive();
+    this.car3.setInteractive();
+    this.car4.setInteractive();
+
+    this.traffic = this.physics.add.group();
+    this.traffic.add(this.car1);
+    this.traffic.add(this.car2);
+    this.traffic.add(this.car3);
+    this.traffic.add(this.car4);
+
+    this.physics.add.overlap(this.player, this.traffic, this.hurtPlayer, null, this);
+
     this.gameOver = false;
 
     // add scoreboard
@@ -139,8 +164,16 @@ class mainScene extends Phaser.Scene {
     this.updateScore();
     this.toggleSound();
     this.toggleMusic();
+    this.moveCar(this.car1)
+    this.moveCar(this.car2)
+    this.moveCar(this.car3)
+    this.moveCar(this.car4)
   }
 
+  hurtPlayer() {
+    console.log("Player has hit a car!")
+    this.gameOver = true
+  }
   movePlayerManager() {
     // Handle all player movement
     // Reset player velocity and angle when no button is being pressed
@@ -194,8 +227,246 @@ class mainScene extends Phaser.Scene {
     }
   }
 
-  spawnCar() {
-    // Spawn a car at a random lane on the road
+  moveCar(car) {
+
+    if (car.x == 229 || car.x == 353) {
+      car.y += 5 * 1.5 + (Phaser.Math.Between(0, 3))
+    }
+    else {
+      car.y += 5 + (Phaser.Math.Between(0, 3))
+    }
+
+    if (car.y > config.height) {
+      this.resetCarPos(car, 5);
+    }
+  }
+
+  resetCarPos(car, speed) {
+    car.y = 0;
+    var randomX = Phaser.Math.Between(0, 3);
+    var randomCar = Phaser.Math.Between(0, 48);
+
+    switch (randomCar) {
+      case 0:
+        car.setFrame("dumptruck")
+        car.setScale(1.8);
+        break;
+      case 1:
+        car.setFrame("tow_truck")
+        car.setScale(2.5);
+        break;
+      case 2:
+        car.setFrame("tow_truck2")
+        car.setScale(2.5);
+        break;
+      case 3:
+        car.setFrame("tow_truck3")
+        car.setScale(2.5);
+        break;
+      case 4:
+        car.setFrame("truck2")
+        car.setScale(2.5);
+        break;
+      case 5:
+        car.setFrame("truck3")
+        car.setScale(2.5);
+        break;
+      case 6:
+        car.setFrame("landcruiser")
+        car.setScale(2.5);
+        break;
+      case 7:
+        car.setFrame("landcruiser2")
+        car.setScale(2.5);
+        break;
+      case 8:
+        car.setFrame("landcruiser3")
+        car.setScale(2.5);
+        break;
+      case 9:
+        car.setFrame("van");
+        car.setScale(2.5);
+        break;
+      case 10:
+        car.setFrame("raptor");
+        car.setScale(2.5);
+        break;
+      case 11:
+        car.setFrame("raptor2");
+        car.setScale(2.5);
+        break;
+      case 12:
+        car.setFrame("pickup");
+        car.setScale(2.5);
+        break;
+      case 13:
+        car.setFrame("pickup2");
+        car.setScale(2.5);
+        break;
+      case 14:
+        car.setFrame("pickup3");
+        car.setScale(2.5);
+        break;
+      case 15:
+        car.setFrame("suv");
+        car.setScale(2.5);
+        break;
+      case 16:
+        car.setFrame("suv2");
+        car.setScale(2.5);
+        break;
+      case 17:
+        car.setFrame("van2");
+        car.setScale(2.5);
+        break;
+      case 18:
+        car.setFrame("van3");
+        car.setScale(2.5);
+        break;
+      case 19:
+        car.setFrame("mustang2");
+        car.setScale(2.5);
+        break;
+      case 20:
+        car.setFrame("camaro");
+        car.setScale(2.5);
+        break;
+      case 21:
+        car.setFrame("camaro2");
+        car.setScale(2.5);
+        break;
+      case 22:
+        car.setFrame("challenger2");
+        car.setScale(2.5);
+        break;
+      case 23:
+        car.setFrame("challenger3");
+        car.setScale(2.5);
+        break;
+      case 24:
+        car.setFrame("lexus");
+        car.setScale(2.5);
+        break;
+      case 25:
+        car.setFrame("lexus2");
+        car.setScale(2.5);
+        break;
+      case 26:
+        car.setFrame("gwagon");
+        car.setScale(2.5);
+        break;
+      case 27:
+        car.setFrame("bmw");
+        car.setScale(2.5);
+        break;
+      case 28:
+        car.setFrame("gwagon2");
+        car.setScale(2.5);
+        break;
+      case 29:
+        car.setFrame("patrol");
+        car.setScale(2.5);
+        break;
+      case 30:
+        car.setFrame("patrol2");
+        car.setScale(2.5);
+        break;
+      case 31:
+        car.setFrame("lexus3");
+        car.setScale(2.5);
+        break;
+      case 32:
+        car.setFrame("taxi");
+        car.setScale(2.5);
+        break;
+      case 33:
+        car.setFrame("taxi2");
+        car.setScale(2.5);
+        break;
+      case 34:
+        car.setFrame("lambo2");
+        car.setScale(2.5);
+        break;
+      case 35:
+        car.setFrame("lancer");
+        car.setScale(2.5);
+        break;
+      case 36:
+        car.setFrame("bmw2");
+        car.setScale(2.5);
+        break;
+      case 37:
+        car.setFrame("bmw3");
+        car.setScale(2.5);
+        break;
+      case 38:
+        car.setFrame("lancer2");
+        car.setScale(2.5);
+        break;
+      case 39:
+        car.setFrame("mustang3");
+        car.setScale(2.5);
+        break;
+      case 40:
+        car.setFrame("mini");
+        car.setScale(2.5);
+        break;
+      case 41:
+        car.setFrame("tida2");
+        car.setScale(2.5);
+        break;
+      case 42:
+        car.setFrame("tida3");
+        car.setScale(2.5);
+        break;
+      case 43:
+        car.setFrame("convertible");
+        car.setScale(2.5);
+        break;
+      case 44:
+        car.setFrame("figo");
+        car.setScale(2.5);
+        break;
+      case 45:
+        car.setFrame("figo2");
+        car.setScale(2.5);
+        break;
+      case 46:
+        car.setFrame("porsche");
+        car.setScale(2.5);
+        break;
+      case 47:
+        car.setFrame("bike2");
+        car.setScale(2.5);
+        break;
+      case 48:
+        car.setFrame("bike");
+        car.setScale(2.5);
+        break;
+    }
+
+    switch (randomX) {
+      case 0:
+        car.x = 229
+        car.y += speed
+        car.setAngle(180)
+        break;
+      case 1:
+        car.x = 353
+        car.y += speed
+        car.setAngle(180)
+        break;
+      case 2:
+        car.x = 478
+        car.y += speed
+        car.setAngle(0)
+        break;
+      case 3:
+        car.x = 611
+        car.y += speed
+        car.setAngle(0)
+        break;
+    }
   }
 
   zeroPad(number, size) {
